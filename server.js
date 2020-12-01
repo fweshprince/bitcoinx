4a4c342e8628d376425942ac2094eb009ecf9d98
fweshprince <j.inglewood007@gmail.com>
2020-12-01 03:41:27 +0100

    security question page fixed

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ mongoose.connection
   .on("error", () => {
     console.log("Error connecting to database");
   });
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log("Listening on " + process.env.PORT));
